360d11c150daee20e63f580355d9ff796e706d91
Lebedev Sergey <barabashka@altlinux.org>
2009-04-16 14:38:49 +0400

    - rewrote qt ui - added custom field

--- alterator-auth/ui/auth/index.scm
+++ alterator-auth/ui/auth/index.scm
@@ -1,55 +1,52 @@
 (document:surround "/std/frame")
-(document:insert "/std/functions")
 
 ;;; Functions
-(define (read-domain)
-  (catch/message
-    (lambda()
-      (domain-id enumref "/auth/avail_domain")
-      (let ((data (woo-read-first "/auth")))
-    (current-id text (woo-get-option data 'current_domain))
-	))))
+(define (update-domain)
+  (let ((domain (form-value "domain")))
+    (form-update-visibility '("domain_name") (string=? domain "custom"))))
 
-(define (write-domain)
+(define (ui-commit)
   (catch/message
     (lambda()
-      (woo-write "/auth"
-		 'domain (domain-id value)
-		 'domain_name (domain-name value))
-      (read-domain)
-      )))
+      (apply woo-write
+	     "/auth"
+	     (form-value-list))
+      (form-update-value-list '("current_domain") (woo-read-first "/auth")))))
+
+(define (ui-init)
+    (let ((data (woo-read-first "/auth")))
+    (form-update-value-list '("current_domain") data)
+    (form-update-enum "domain" (woo-list "/auth/avail_domain"))
+    (update-domain)))
 
 ;;; UI
 (gridbox
-  columns "0;100"
-  margin 50
+    columns "0;100"
+    margin 50
 
-  (label text (_ "Current domain:") align "right")
-  (document:id current-id (label))
+    (label text (_ "Current domain:") align "right")
+    (label name "current_domain")
 
-  (label colspan 2)
+    (label colspan 2)
 
-  (label text (_ "Domain list:") align "right")
-  (document:id domain-id (combobox name "domain"))
-
-  (label colspan 2)
-
-  (label text (_ "Enter Domain:") align "right" name "domain_name")
-  (document:id domain-name (edit name "domain_name"))
-
-
-  (label colspan 4)
-
+    (label text (_ "Domain list:") align "right")
+    (combobox name "domain")
+    
+    (spacer)
+    (edit name "domain_name" visibility #t)
+    
+    (label colspan 2)
+    
     (if (global 'frame:next)
     (label)
     (hbox align "left"
-	(button text (_ "Apply") (when clicked (write-domain)))))
-  (spacer)
- 
-  )
+	(button name "apply" text (_ "Apply") (when clicked (ui-commit)))))
+)
 
 ;;; Logic
 
 (document:root
   (when loaded
-    (read-domain)))
+    (ui-init)
+    (form-bind "domain" "change" update-domain)
+    (form-bind "apply" "click" ui-commit)))
